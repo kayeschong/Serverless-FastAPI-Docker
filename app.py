450a59a6142ee62f2119534e6a2498ab73ebf240
Yu Kai <kayeschong@gmail.com>
2021-02-18 00:20:11 +0800

    Add Pydantic MatchData model for validation, remove empty pattern checks for simplicity

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 # coding: utf8
 from fastapi import FastAPI
+from pydantic import BaseModel
 import spacy
 from spacy.matcher import Matcher
+from typing import List, Dict
+
 
 app = FastAPI()
 
 MODELS = {"en_core_web_sm": spacy.load("en_core_web_sm")}
+
+
+class MatchData(BaseModel):
+    text: str
+    # Pattern format changed for spacy v3
+    pattern: List[
+        List[Dict[str, str]]
+    ]
 
 
 def get_model_desc(nlp, model_name):
@@ -22,26 +33,28 @@ def models():
 
 
 @app.post("/match")
-def match(text: str, model: str, pattern: list):
+def match(model: str, data: MatchData):
     nlp = MODELS[model]
-    if pattern:
-        matcher = Matcher(nlp.vocab)
-        matcher.add("PATTERN", pattern) # pattern API changed for v3
-    doc = nlp(text)
+
+    matcher = Matcher(nlp.vocab)
+    matcher.add("PATTERN", data.pattern) # pattern args changed for v3
+    
+    doc = nlp(data.text)
     tokens = []
     matches = []
     match_tokens = set()
-    if pattern:
-        for _, start, end in matcher(doc):
-            if start >= end:  # filter out null matches or results of weird bug
-                continue
-            span = doc[start:end]
-            if span[0].i in match_tokens:  # filter out overlaps
-                continue
-            match_tokens.update([t.i for t in span])
-            matches.append(
-                {"start": span.start_char, "end": span.end_char, "label": "MATCH"}
-            )
+
+    for _, start, end in matcher(doc):
+        if start >= end:  # filter out null matches or results of weird bug
+            continue
+        span = doc[start:end]
+        if span[0].i in match_tokens:  # filter out overlaps
+            continue
+        match_tokens.update([t.i for t in span])
+        matches.append(
+            {"start": span.start_char, "end": span.end_char, "label": "MATCH"}
+        )
+
     for t in doc:
         start = t.idx
         end = t.idx + len(t.text)
